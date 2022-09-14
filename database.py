7b1dbe6f73fa43bb80b2261333125445d37be132
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2022-09-15 00:23:51 +0900

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 
 
 
