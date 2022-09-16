41da2db09306463e5ac4a45b373d6acff2cb86af
JeongHunHui <deca3248@gmail.com>
2022-09-16 22:27:52 +0900

    fix: 삭제 api put->delete 변경

--- main.py
+++ main.py
@@ -152,7 +152,7 @@ def show_user(user_id: int, db: Session = Depends(get_db)):
 
 
 # user_id를 path variable로 받아서 해당 유저를 soft delete
-@app.put('/api/v1/users/{user_id}', response_model=schemas.User)
+@app.delete('/api/v1/users/{user_id}', response_model=schemas.User)
 def delete_user(user_id: int, db: Session = Depends(get_db)):
     return crud.delete_user(db=db, user_id=user_id)
 
