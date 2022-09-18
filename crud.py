1d38ac5e5c651125074c06e8841783c6200a0fe4
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2022-09-18 19:45:58 +0900

    Update crud.py

--- crud.py
+++ crud.py
@@ -1,7 +1,6 @@
 # crud.py
 # db에 직접 접근하여 create, read, update, delete 하는 함수를 관리하는 파일
 
-from pyexpat import model
 from typing import List
 from sqlalchemy.orm import Session
 from fastapi import HTTPException
